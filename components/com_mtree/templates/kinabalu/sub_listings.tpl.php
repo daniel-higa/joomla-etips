593df989efe13c0412d273725adfd737c10502fa
daniel higa <daniel.higa@bugmenot.com>
2012-11-29 00:10:37 -0300

    +add like and tweet to cities

--- components/com_mtree/templates/kinabalu/sub_listings.tpl.php
+++ components/com_mtree/templates/kinabalu/sub_listings.tpl.php
@@ -40,3 +40,18 @@ if( $this->task == "search" && empty($this->links) ) {
 	}
 }
 ?></div>
+<div style="float:left; margin-left:300px;">
+<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
+<div style="float:left;">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-send="false" data-layout="button_count" data-width="150" data-show-faces="false"></div>
+</div>
